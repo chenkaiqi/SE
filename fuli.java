1b847fbe852f9072b80b0c37643e7b2387ba997a
chenkaiqi <200892339@qq.com>
2016-03-09 23:34:27 +0800

    this is the first commit

---
+++ fuli.java
@@ -0,0 +1,13 @@
+package ch1;
+
+public class Calculate{ 
+	public static void main(String[] args){ 
+		double money = 1000;  //本金  
+		int years = 20;   //存钱年数
+		double sum = 0; //存款总额 
+		double r = 0.05;  //利率 
+		for(int i = 1 ; i<=years; i++){  
+			sum = (1+ r) * i *money;   
+			System.out.println("存入第"+i+"年后的存款总额为："+sum);  }
+		}
+	}
